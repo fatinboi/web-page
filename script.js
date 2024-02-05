bfb1a547533626c7b333ae920d147ca95ab04b3e
fatinboi <137381954+fatinboi@users.noreply.github.com>
2024-02-06 01:07:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ async function fetchProducts(category) {
     const response = await fetch('https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json');
     const data = await response.json();
     
-    if (data && data[category]) {
+    if (data && data[category] && Array.isArray(data[category])) {
       return data[category];
     } else {
-      console.error('No products found for category:', category);
+      console.error('Invalid or missing data for category:', category);
       return [];
     }
   } catch (error) {
